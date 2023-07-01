332cf000c120c4faf1c467949681a2df857588f2
H-C-21 <harshit.c21@iiits.in>
2023-07-01 21:20:49 +0530

    f

--- src/pages/AttributeSelector.js
+++ src/pages/AttributeSelector.js
@@ -2,28 +2,49 @@ import { Fragment } from "react";
 import { Slider } from "@mui/material";
 
 import classes from "./AttributeSelector.module.css"
+import { useEffect } from "react";
+import { useState } from "react";
 
 function AttributeSelector(props){
 
+    const [index,setIndex] = useState("Literacy Rate")
     function yearChangeHandler(event){
         props.onSelect({prop:props.attribute.prop,year:event.target.value})
     }
+
+    useEffect(()=>{
+        if(props.attribute.prop === 'sratio'){
+          setIndex('Sex Ratio')
+        }
+        else if(props.attribute.prop === 'lit'){
+          setIndex('Literacy Rate')
+        }
+        else if(props.attribute.prop === 'den'){
+            setIndex('Population Density')
+        }
+        else{
+          setIndex('Population')
+        }
+    },[props.attribute.prop])
     
     return (
       <Fragment>
         <div className={classes.maincontainer}>
-          <h2>Select your Parameter</h2>
+          <h2>Select Index</h2>
+          <div className={classes.info}>Indian Map showing {index} by state in the year {props.attribute.year}. 
+          Hover over a state to see the details.</div>
+          <div className={classes.select}>
           <select
             name="attribute"
             onChange={props.ach}
             value={props.attribute.prop}
             id = 'attri'
           >
-            <option value="lit">Literacy Rate</option>
-            <option value="pop">Population</option>
-            <option value="den">Population Density</option>
-            <option value="sratio">Sex Ratio</option>
-          </select>
+            <option value="lit"><p>Literacy Rate</p></option>
+            <option value="pop"><p>Population</p></option>
+            <option value="den"><p>Population Density</p></option>
+            <option value="sratio"><p>Sex Ratio</p></option>
+          </select></div>
           <div className={classes.slider1}>
           <Slider min = {1951} max ={2011}
             defaultValue={2011} step = {10}
